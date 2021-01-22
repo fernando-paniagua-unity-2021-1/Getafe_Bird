9f3803927b2214c575f164ef3690c8b8e75d8879
Fernando Paniagua <fernando.paniagua@gmail.com>
2021-01-22 20:54:59 +0100

    Initial commit

---
+++ Assets/_GameAssets/Scripts/Pajaro.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pajaro : MonoBehaviour
+{
+    public Rigidbody parteFisica;//Declaración del componente rigidbody
+
+    [Range(1,10)]
+    public float velocidad;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X)){
+            //parteFisica.AddForce(Vector3.up * 500);
+            parteFisica.velocity = new Vector3(0, velocidad, 0);
+        }
+    }
+}
