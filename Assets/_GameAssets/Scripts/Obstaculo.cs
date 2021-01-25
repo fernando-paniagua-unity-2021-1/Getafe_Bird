cf678fd1102d4df5e5f666232e68a0610770b3f3
Fernando Paniagua <fernando.paniagua@gmail.com>
2021-01-25 21:02:35 +0100

    Sesion 2021-01-25

---
+++ Assets/_GameAssets/Scripts/Obstaculo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstaculo : MonoBehaviour
+{
+    public float speed;
+    void Update()
+    {
+        //Time.deltaTime Tiempo que ha transcurrido desde el último frame
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
+    }
+}
