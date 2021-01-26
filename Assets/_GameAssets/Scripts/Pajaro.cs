8c50dbaa266d6a0a25ec4a46f7b96d9608b07efd
Fernando Paniagua <fernando.paniagua@gmail.com>
2021-01-26 21:03:55 +0100

    Sesion 2021-01-26
    Agregado sistema de partículas
    Agregado sonido de trompazo
    Añadido "spawn" de obstáculos

--- Assets/_GameAssets/Scripts/Pajaro.cs
+++ Assets/_GameAssets/Scripts/Pajaro.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Pajaro : MonoBehaviour
 {
     public Rigidbody parteFisica;//Declaración del componente rigidbody
-
+    public GameObject prefabSangre;
     [Range(1,10)]
     public float velocidad;
 
@@ -15,5 +15,13 @@ public class Pajaro : MonoBehaviour
             //parteFisica.AddForce(Vector3.up * 500);
             parteFisica.velocity = new Vector3(0, velocidad, 0);
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Crea una copia del prefab de la explosión en la posición y con la rotación del Pajaro
+        Instantiate(prefabSangre, transform.position, transform.rotation);
+        //Destruye el pájaro
+        Destroy(gameObject);
     }
 }
---
+++ Assets/_GameAssets/Scripts/Spawn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject prefabObstaculo;
+    public float pausaEntreSpawn;
+
+    void Start()
+    {
+        InvokeRepeating("CrearObstaculo", 0, pausaEntreSpawn);
+    }
+
+    public void CrearObstaculo()
+    {
+        Instantiate(prefabObstaculo, transform);
+    }
+}
