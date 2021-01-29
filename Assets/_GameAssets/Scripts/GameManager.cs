58318d3c9becbdda95dd2ac19fb78e823dfb215e
Fernando Paniagua <fernando.paniagua@gmail.com>
2021-01-29 20:48:25 +0100

    Fin del juego

--- Assets/_GameAssets/Scripts/GameManager.cs
+++ Assets/_GameAssets/Scripts/GameManager.cs
@@ -7,10 +7,52 @@ public class GameManager : MonoBehaviour
 {
     public int puntuacion=0;
     public Text textoPunt;
+    public Text textoMax;
+    public Text textoComenzar;
+    public Text textoPausa;
+    private int maximo;
+    private void Awake()
+    {
+        //Pausamos el juego
+        Time.timeScale = 0;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
 
+        if (Time.timeScale == 0 && Input.GetKeyDown(KeyCode.X))
+        {
+            Time.timeScale = 1;
+            textoComenzar.enabled = false;
+            textoPausa.enabled = false;
+        }
+        if (Time.timeScale == 1 && Input.GetKeyDown(KeyCode.P))
+        {
+            Time.timeScale = 0;
+            textoPausa.enabled = true;
+        }
+    }
+    private void Start()
+    {
+        //Leer el máximo almacenado (si no existe se le asigna 0)
+        maximo = PlayerPrefs.GetInt("Maximo", 0);
+        //Mostrar en el campo del UI el texto
+        textoMax.text = "Max.:" + maximo;
+    }
     public void AgregarPunto()
     {
         puntuacion++;
         textoPunt.text = puntuacion.ToString();
+    }
+    public void GuardarPuntuacionMaxima()
+    {
+        if (puntuacion > maximo)
+        {
+            PlayerPrefs.SetInt("Maximo", puntuacion);
+            PlayerPrefs.Save();
+        }
     }
 }
--- Assets/_GameAssets/Scripts/Pajaro.cs
+++ Assets/_GameAssets/Scripts/Pajaro.cs
@@ -11,9 +11,13 @@ public class Pajaro : MonoBehaviour
     public GameObject prefabSangre;
     [Range(1,10)]
     public float velocidad;
+    [Range(1, 10)]
+    public float velocidadRotacion;
 
     void Update()
     {
+        transform.rotation = Quaternion.Euler(0, 0, parteFisica.velocity.y * velocidadRotacion);
+
         if (Input.GetKeyDown(KeyCode.X)){
             //parteFisica.AddForce(Vector3.up * 500);
             parteFisica.velocity = new Vector3(0, velocidad, 0);
@@ -23,6 +27,9 @@ public class Pajaro : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        //Guarda la puntuación máxima
+        GameManager gameManager = GameObject.Find("GameManagerObjeto").GetComponent<GameManager>();
+        gameManager.GuardarPuntuacionMaxima();
         //Crea una copia del prefab de la explosión en la posición y con la rotación del Pajaro
         Instantiate(prefabSangre, transform.position, transform.rotation);
         //Destruye el pájaro
